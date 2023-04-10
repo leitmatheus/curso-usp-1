d2a5350f61a731cb82b2683a663a1915b9624d3d
Matheus Leite <leitmatheus@gmail.com>
2023-04-10 20:19:30 -0300

    fatura - usp

---
+++ projeto-1/fatura.py
@@ -0,0 +1,6 @@
+nomeCliente = input("Digite o nome do cliente: ")
+diaVencimento = input("Digite o dia de vencimento: ")
+mêsVencimento = input("Digite o mês de vencimento: ")
+valorFatura = input("Digite o valor da fatura: ")
+
+print("Olá,", nomeCliente, "\n" "A sua fatura com vencimento em", diaVencimento, "de", mêsVencimento, "no valor de R$", valorFatura, "está fechada")
