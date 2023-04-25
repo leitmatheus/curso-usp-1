2a9fbb462a814bfdaf77ef73feee514aca2b5438
Matheus Leite <leitmatheus@gmail.com>
2023-04-25 18:13:07 -0300

    ordenaçao-usp

---
+++ projeto-1/ordenacao.py
@@ -0,0 +1,10 @@
+numero1 = int(input("Defina o primeiro número: "))
+numero2 = int(input("Defina o segundo número: "))
+numero3 = int(input("Defina o terceiro número: "))
+
+if numero1 < numero2 and numero2 < numero3:
+  print("Crescente")
+else:
+  print("Não está em ordem crescente")
+
+  
